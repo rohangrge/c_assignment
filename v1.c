ae76effa6a2e8e223f9878756740d0af0578183e
rohangrge <57232256+rohangrge@users.noreply.github.com>
2020-05-10 11:27:02 +0530

    Initial commit

---
+++ v1.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<string.h>
+void cmd();
+void chcmd();
+int main(void)
+{
+    cmd();
+}
+
+
+
+void cmd()
+{   
+    char com[50];
+    printf("Enter your command\n");
+    scanf("%s",com);
+    char a[50]="passwd";
+    
+    if(strcmp(a,com)==0)
+    {
+        printf("You have entered the right command");
+    }
+    else
+    {
+      cmd();
+    }
+}
+void pas()
+{
+    
